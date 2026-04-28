4054332eef647b1ca963d30db2948df128db0cea
fredyirawannasution <fredyirawannasution@gmail.com>
2026-04-28 15:09:18 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Ganti URL ini dengan URL deployment Google Apps Script Web App Anda.
 // Contoh format: https://script.google.com/macros/s/AKfycbxxxxxxxxxxxxxxxx/exec
 window.TEKHUM_CONFIG = {
-  APP_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbwSiEkzh7xqKH8d1WRcGf0fSIdIdoe5MtOZ8JDqQZ8CL18FgBBYYyezJvNmfWsjn6Gn/exec',
-  APP_TITLE: 'Pengelolaan Dokumen Teknis & Hukum',
+  APP_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzfAEDK1bN9amjAs7-1BbS75c0ftEFJEv1r0vqvf5FZc18yhAmh7OBs6dqY3hpvUt69AA/exec',
+  APP_TITLE: 'Pengelolaan Arsip Data Dukung E-Lapkin',
   ORG_NAME: 'KPU Kabupaten Labuhanbatu Utara'
 };
